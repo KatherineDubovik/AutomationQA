5d80ed082047ae309dbc8d55710a72f0e30aa1db
Katherine Dubovik <dubovik1505@gmail.com>
2022-08-24 11:31:59 +0300

    hw19. Adding explicit waits

--- hw19/selenium/selenium.test.ts
+++ hw19/selenium/selenium.test.ts
@@ -8,7 +8,6 @@ const defaultWaitingTime = 5000;
 const baseUrl = "https://www.21vek.by/";
 
 const defaultItemQuantity = 1;
-let itemsQuantity: string;
 
 
 describe("Tests for 21vek.by site", () => {
@@ -22,9 +21,8 @@ describe("Tests for 21vek.by site", () => {
     test("Should follow the page for a definite category", async () => {
         const followingPageAddress = "mobile/";
         await driver.findElement(By.linkText("Смартфоны")).click();
-        const currentUrl = await driver.getCurrentUrl();
+        await driver.wait(until.urlIs(`${baseUrl}${followingPageAddress}`));
         const header = await driver.findElement(By.css(".content__header")).getText();
-        expect(currentUrl).toBe(`${baseUrl}${followingPageAddress}`);
         expect(header).toStrictEqual("Смартфоны");
     });
 
@@ -42,29 +40,29 @@ describe("Tests for 21vek.by site", () => {
     test("Should display correct item title and quantity at the cart", async () => {
         const catalogItemTitle = await driver.findElement(By.xpath(`//a[@href="https://www.21vek.by/mobile/redmi9c4gb128gbnfc_xiaomi.html"]/span[2]`)).getText();
         await driver.findElement(By.css(".headerCartLabel")).click();
-        await driver.wait(until.titleIs("Оформление заказа"), defaultWaitingTime);
-        const cartItemTitle = await driver.findElement(By.css(`a[href="https://www.21vek.by/mobile/redmi9c4gb128gbnfc_xiaomi.html"]`)).getText();
-        itemsQuantity = await driver.findElement(By.id("j-counter-8886977")).getAttribute('value');
+        await driver.wait(async () => {
+            return await driver.findElement(By.css(`a[href="https://www.21vek.by/mobile/redmi9c4gb128gbnfc_xiaomi.html"]`)).getText() === catalogItemTitle
+        });
+        const itemsQuantity = await driver.findElement(By.id("j-counter-8886977")).getAttribute('value');
         const cartCounter = await driver.findElement(By.css(".BasketTabsScreen_counter__2iMje")).getText();
-        expect(cartItemTitle).toStrictEqual(catalogItemTitle);
         expect(itemsQuantity).toStrictEqual(String(defaultItemQuantity));
         expect(cartCounter).toStrictEqual("1");
     });
 
     test("Should increase item quantity at the cart", async () => {
         await driver.findElement(By.id("j-plus-8886977")).click();
-        itemsQuantity = await driver.findElement(By.id("j-counter-8886977")).getAttribute('value');
-        await driver.sleep(defaultWaitingTime);
-        expect(itemsQuantity).toStrictEqual(String(defaultItemQuantity + 1));
+        await driver.wait(async () => {
+            return await driver.findElement(By.id("j-counter-8886977")).getAttribute('value') === String(defaultItemQuantity + 1)
+        });
     }); 
 
 
     test("Should delete item from the cart", async () => {
         await driver.findElement(By.linkText("удалить")).click();
-        await driver.sleep(defaultWaitingTime);
-        const emptyCartMessage = await driver.findElement(By.id("b-empty-basket-container")).getText();
+        await driver.wait(async () => {
+            return await driver.findElement(By.id("b-empty-basket-container")).getText() === "Ваша корзина пуста\n← Вернуться к покупкам"
+        });
         const basketContainer = await driver.findElement(By.css("#j-basket-container")).getAttribute("style");
-        expect(emptyCartMessage).toStrictEqual("Ваша корзина пуста\n← Вернуться к покупкам");
         expect(basketContainer).toBe("display: none;");
     }); 
 
